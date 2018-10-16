928a172ffd4e19108454b7c94b320c7b24512373
viktorkuznietsov1986 <victor.kuzn@gmail.com>
2018-10-16 00:29:28 -0400

    updated preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,17 @@
 import cv2
 import os
 
-train_raw_dir = './data/train/raw/'
-train_processed_dir = './data/train/scaled/'
+train_raw_dir = '../../train_data/'
+train_processed_dir = '../../train_data/scaled/'
 
 dim = (500, 500)
 
 raw_files = os.listdir(train_raw_dir)
 
-for file in raw_files:
+for index, file in enumerate(raw_files):
+    if index > 10:
+        break
+
     file_name = train_raw_dir + file
     image = cv2.imread(file_name)
     resized = cv2.resize(image, dim)
