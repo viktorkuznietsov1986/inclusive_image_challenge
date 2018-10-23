76248c52f40f8735d0f059cffc7ad4e889028481
Viktor Kuznietsov <victor.kuzn@gmail.com>
2018-10-23 11:34:44 -0400

    optimized the prediction logic a bit: moved resize operation to the first place

--- build_submission.py
+++ build_submission.py
@@ -35,8 +35,8 @@ def normalize(img):
 
 def predict(model, image_name, threshold=0.1):
     image = cv2.imread(image_name)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (input_shape[0], input_shape[1]))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = normalize(image)
     image = np.reshape(image, [1, input_shape[0], input_shape[1], n_channels])
     prediction = model.predict(image)[0]
