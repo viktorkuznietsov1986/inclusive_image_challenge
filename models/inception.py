22742c0b854c212d245bea658bcdb0f4d0069aaf
viktorkuznietsov1986 <victor.kuzn@gmail.com>
2018-10-23 20:50:30 -0400

    updated the inception based model

--- models/inception.py
+++ models/inception.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Activation, Dense, Flatten, AveragePooling2D, Conv2D, MaxPooling2D, BatchNormalization
+from keras.layers import Activation, Dense, Flatten, GlobalAveragePooling2D, Conv2D, MaxPooling2D, BatchNormalization
 from keras import applications
 
 
@@ -11,7 +11,7 @@ def build_inceptionv3_based_classifier(input_shape, num_classes):
     model = Sequential()
     model.add(inceptionv3)
     model.add(Conv2D(num_classes, kernel_size=(1,1)))
-    model.add(AveragePooling2D())
+    model.add(GlobalAveragePooling2D())
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dense(num_classes))
